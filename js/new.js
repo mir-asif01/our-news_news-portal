8cbac8734d1ac841cdd3b7a8110086f96bfca753
asif3062 <asifbsb2002@gmail.com>
2022-09-02 21:25:47 +0600

    news category section added dynamic

--- js/new.js
+++ js/new.js
@@ -1,1 +1,20 @@
-console.log('Hello News')
+const loadCategories = () =>{
+    fetch('https://openapi.programming-hero.com/api/news/categories')
+    .then(res => res.json())
+    .then(categories => showCategories(categories.data.news_category));
+}
+const showCategories = (categories) =>{
+    for(const category of categories){
+        console.log(category.category_name)
+        const categoryContainer = document.getElementById('categories');
+        const newCategoryDiv = document.createElement('div');
+        newCategoryDiv.innerHTML = `
+        <button class="text-2xl tab">${category.category_name}</button>
+        `
+        categoryContainer.appendChild(newCategoryDiv)
+    }
+}
+
+
+
+loadCategories();
