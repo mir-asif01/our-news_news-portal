e41faa742ff88b85bbdf1ced12c4a6d3a43d109e
asif3062 <asifbsb2002@gmail.com>
2022-09-02 21:46:23 +0600

    got all news by category

--- js/new.js
+++ js/new.js
@@ -1,3 +1,5 @@
+// loading category
+
 const loadCategories = () =>{
     fetch('https://openapi.programming-hero.com/api/news/categories')
     .then(res => res.json())
@@ -5,16 +7,30 @@ const loadCategories = () =>{
 }
 const showCategories = (categories) =>{
     for(const category of categories){
-        console.log(category.category_name)
+        // console.log(category.category_id)
         const categoryContainer = document.getElementById('categories');
         const newCategoryDiv = document.createElement('div');
         newCategoryDiv.innerHTML = `
-        <button class="text-2xl tab">${category.category_name}</button>
+        <button class="text-2xl tab" onclick="loadNews(${category.category_id})" >${category.category_name}</button>
         `
         categoryContainer.appendChild(newCategoryDiv)
     }
 }
-
-
-
 loadCategories();
+
+///////////////////////////////////////////////
+// show news function
+
+const loadNews = (category) =>{
+    // console.log(category);
+    const url =`https://openapi.programming-hero.com/api/news/category/0${category}`
+    fetch(url)
+    .then(res => res.json())
+    .then(allNews => showNews(allNews.data));
+}
+
+const showNews = (newsArray) =>{
+    for(const news of newsArray){
+        console.log(news.total_view ? news.total_view : 'no data')
+    }
+}
